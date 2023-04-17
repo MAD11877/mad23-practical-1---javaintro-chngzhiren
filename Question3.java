69e8456685960389a3c7d615e40f287d568dad04
chngzhiren <130948722+chngzhiren@users.noreply.github.com>
2023-04-17 11:44:45 +0800

    Update Question3.java

--- Question3.java
+++ Question3.java
@@ -11,6 +11,10 @@ public class Question3
      */
      
     Scanner in = new Scanner(System.in);
-    
+      System.out.println("Please enter a number: ");
+    int num = in.nextInt();
+    int newnum = num * num;
+      System.out.println(newnum);
+  } 
   }
 }
